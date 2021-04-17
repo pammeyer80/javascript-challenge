9ea28f4e410a54cc6b23d1496ab706555eede78f
Pam M <22107270+pammeyer80@users.noreply.github.com>
2021-04-16 22:55:54 -0500

    get data loaded into initial table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Get d3 elements
+var button = d3.select("#filter-btn");
+var form = d3.select("#ufo-form");
+
+// Console.log the ufo data from data.js
+console.log(tableData);
+
+// Create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+//Initialize ufo data table
+data.forEach((ufoData) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// Event handler for form
+function runEnter(){
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+
+};

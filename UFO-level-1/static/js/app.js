f00a7d33aa92109d4f9fae9a14fa871f43ee0998
Pam M <22107270+pammeyer80@users.noreply.github.com>
2021-04-16 23:51:26 -0500

    add date input filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -16,6 +16,7 @@ button.on("click", runEnter);
 form.on("submit", runEnter);
 
 //Initialize ufo data table
+//function loadUfoData
 data.forEach((ufoData) => {
     var row = tbody.append("tr");
     Object.entries(ufoData).forEach(([key, value]) => {
@@ -27,8 +28,30 @@ data.forEach((ufoData) => {
 
 // Event handler for form
 function runEnter(){
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
 
+    // Get input value
+    var dateInput = d3.select("#datetime").property("value");
+
+    // check value
+    console.log(dateInput);
+
+    // filter data based on input value
+    var filteredUfoData = data.filter(ufo => (ufo.datetime === dateInput) );
+
+    // check filtered data
+    console.log(filteredUfoData);
+
+    // clear html body
+    tbody.html("");
+
+    // repopulate table based on filter
+    filteredUfoData.forEach((data) => {
+        var row = tbody.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
 };
